6ec250733d9558af3d3106d77bf90d998396f8e8
Guillermo Fernández <guillermofr@vauxoo.com>
2023-11-30 23:31:09 -0600

    solved day 1 part 1

--- day1/part1.go
+++ day1/part1.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 func partOne() {
@@ -11,7 +14,45 @@ func partOne() {
 		panic(err)
 	}
 
-	_ = inputData
+	lines := strings.Split(string(inputData), "\n")
 
-	fmt.Println("preparing for aoc2023")
+	count := 0
+
+	for _, line := range lines {
+		firstIndex := strings.IndexFunc(line, func(letter rune) bool {
+			match, err := regexp.MatchString(`[0-9]`, string(letter))
+			if err != nil {
+				panic(err)
+			}
+
+			return match
+		})
+
+		lastIndex := strings.LastIndexFunc(line, func(letter rune) bool {
+			match, err := regexp.MatchString(`[0-9]`, string(letter))
+			if err != nil {
+				panic(err)
+			}
+
+			return match
+		})
+
+		if firstIndex < 0 || lastIndex < 0 {
+			fmt.Println(line)
+			panic("wtf")
+		}
+
+		d := string(line[firstIndex]) + string(line[lastIndex])
+
+		digit, err := strconv.Atoi(d)
+		if err != nil {
+			panic(err)
+		}
+
+		count += digit
+
+	}
+
+	fmt.Println(count)
+
 }
