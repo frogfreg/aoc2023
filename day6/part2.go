8d0a8fddcf292d8f96d02b693fd5a2c58acc8cd9
Guillermo Fernandez <guillermofr@vauxoo.com>
2023-12-20 20:29:56 -0600

    finished day 6 part 2

--- day6/part2.go
+++ day6/part2.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func partTwo() {
+	inputData, err := os.ReadFile("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(inputData), "\n")
+
+	timeStr := strings.Join(strings.Fields(strings.TrimPrefix(lines[0], "Time:")), "")
+	distanceStr := strings.Join(strings.Fields(strings.TrimPrefix(lines[1], "Distance:")), "")
+
+	time, _ := strconv.Atoi(timeStr)
+	distance, _ := strconv.Atoi(distanceStr)
+
+	fmt.Println(waysToWin(time, distance))
+}
